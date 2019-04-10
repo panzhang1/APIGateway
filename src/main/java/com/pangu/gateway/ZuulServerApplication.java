c27662c1ad06dac443de42c46af59b6caf2a668a
Zhang, Pan <pan.zhang01@sap.com>
2019-04-10 15:11:41 +0800

    oauth

--- src/main/java/com/pangu/gateway/ZuulServerApplication.java
+++ src/main/java/com/pangu/gateway/ZuulServerApplication.java
@@ -3,9 +3,11 @@ package com.pangu.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableResourceServer
 public class ZuulServerApplication {
    
     public static void main(String[] args) {
