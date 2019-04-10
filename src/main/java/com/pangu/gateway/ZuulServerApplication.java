b970d651bb61375209f5b91998c717f60cd6b44c
Zhang, Pan <pan.zhang01@sap.com>
2019-04-10 14:54:15 +0800

    1

--- src/main/java/com/pangu/gateway/ZuulServerApplication.java
+++ src/main/java/com/pangu/gateway/ZuulServerApplication.java
@@ -1,13 +1,14 @@
 package com.pangu.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulServerApplication {
-    
+   
     public static void main(String[] args) {
-    SpringApplication.run(ZuulServerApplication.class, args);
-} }
+        SpringApplication.run(ZuulServerApplication.class, args);
+    } 
+}
